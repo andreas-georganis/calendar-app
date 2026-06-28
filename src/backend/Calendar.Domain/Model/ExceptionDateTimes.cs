caa694476597364ca10df1fdf344cd56823c451f
andreasgeorganis <georganis.andrew@gmail.com>
2026-06-28 22:25:22 +0300

    Redesign Recurrence date times

--- src/backend/Calendar.Domain/Model/ExceptionDateTimes.cs
+++ src/backend/Calendar.Domain/Model/ExceptionDateTimes.cs
@@ -4,23 +4,23 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Calendar.Domain.Model;
 
-public sealed class ExceptionDates : IEnumerable<DateTime>, IParsable<ExceptionDates>
+public sealed class ExceptionDateTimes : IEnumerable<DateTime>, IParsable<ExceptionDateTimes>
 {
-    static ExceptionDates Empty => new ExceptionDates(ImmutableList<DateTime>.Empty);
+    static ExceptionDateTimes Empty => new ExceptionDateTimes(ImmutableList<DateTime>.Empty);
     
     private readonly ImmutableList<DateTime> _values;
     
-    public ExceptionDates(IEnumerable<DateTime> values)
+    public ExceptionDateTimes(IEnumerable<DateTime> values)
     {
         _values = values.ToImmutableList();
     }
     
-    public ExceptionDates Add(DateTime value)
-        => new ExceptionDates(_values.Add(value));
+    public ExceptionDateTimes Add(DateTime value)
+        => new ExceptionDateTimes(_values.Add(value));
     
     
-    public ExceptionDates AddRange(IEnumerable<DateTime> values)
-        => new ExceptionDates(_values.AddRange(values));
+    public ExceptionDateTimes AddRange(IEnumerable<DateTime> values)
+        => new ExceptionDateTimes(_values.AddRange(values));
     
     public IEnumerator<DateTime> GetEnumerator()
     {
@@ -32,10 +32,10 @@ public sealed class ExceptionDates : IEnumerable<DateTime>, IParsable<ExceptionD
         return GetEnumerator();
     }
 
-    public static ExceptionDates Parse(string s, IFormatProvider? provider)
+    public static ExceptionDateTimes Parse(string s, IFormatProvider? provider)
         => TryParse(s, provider, out var result) ? result : throw new FormatException();
 
-    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out ExceptionDates result)
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out ExceptionDateTimes result)
     {
         if (string.IsNullOrWhiteSpace(s))
         {
@@ -57,7 +57,7 @@ public sealed class ExceptionDates : IEnumerable<DateTime>, IParsable<ExceptionD
             values.Add(dateTime);
         }
         
-        result = new ExceptionDates(values);
+        result = new ExceptionDateTimes(values);
         return true;
     }
 }
---
+++ src/backend/Calendar.Domain/Model/RecurrenceDateTimes.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Calendar.Domain.Model;
+
+public sealed class RecurrenceDateTimes : IEnumerable<Period>, IParsable<RecurrenceDateTimes>
+{
+    static RecurrenceDateTimes Empty => new RecurrenceDateTimes(ImmutableList<Period>.Empty);
+    
+    private readonly ImmutableList<Period> _values;
+
+    public RecurrenceDateTimes(IEnumerable<Period> values)
+    {
+        _values = values.ToImmutableList();
+    }
+    
+    public RecurrenceDateTimes Add(Period value)
+        => new RecurrenceDateTimes(_values.Add(value)); 
+    
+    public RecurrenceDateTimes AddRange(IEnumerable<Period> values)
+        => new RecurrenceDateTimes(_values.AddRange(values));
+
+    public IEnumerator<Period> GetEnumerator()
+    {
+        return _values.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public static RecurrenceDateTimes Parse(string s, IFormatProvider? provider)
+        => TryParse(s, provider, out var result) ? result : throw new FormatException();
+
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out RecurrenceDateTimes result)
+    {
+        if (string.IsNullOrWhiteSpace(s))
+        {
+            result = Empty;
+            return true;
+        }
+        
+        var segments = s.Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        
+        var values = new List<Period>();
+        foreach (var segment in segments)
+        {
+            if (!Period.TryParse(segment, provider, out var period))
+            {
+                result = Empty;
+                return false;
+            }
+            
+            values.Add(period); 
+        }
+
+        result = new RecurrenceDateTimes(values);
+        return true;
+    }
+}
